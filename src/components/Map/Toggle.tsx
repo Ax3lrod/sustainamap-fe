4c4ebe5d684580bbc029366e52358cce97a757b4
fioreenza <fiorenzanalle05@gmail.com>
2025-08-06 11:19:15 +0700

    fix: dependencies react query

--- src/components/Map/Toggle.tsx
+++ src/components/Map/Toggle.tsx
@@ -2,10 +2,22 @@ import Typography from "../Typography";
 
 export default function Toggle () {
     return (
-        <div className="bg-secondary w-64 h-fit px-10 absolute top-64 z-10 right-0">
-                <Typography className="bg-secondary border-1 border-primary-600">
+        <div className="bg-secondary w-56 space-y-10 h-fit px-10 absolute top-64 z-10 right-0">
+            <button className="bg-secondary px-4 rounded-full border-1 border-primary-600">
+                <Typography variant="bs" font="body">
                     Kualitas Udara
                 </Typography>
+            </button>
+             <button className="bg-secondary px-4 rounded-full border-1 border-primary-600">
+                <Typography variant="bs" font="body">
+                    Kualitas Udara
+                </Typography>
+            </button>
+            <button className="bg-secondary px-4 rounded-full border-1 border-primary-600">
+                <Typography variant="bs" font="body">
+                    Kualitas Udara
+                </Typography>
+            </button>
         </div>
     )
 }
