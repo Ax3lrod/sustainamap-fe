2f6631cd8807768e9cce6cc374742d8bef67a2dc
fioreenza <fiorenzanalle05@gmail.com>
2025-08-06 09:07:00 +0700

    feat: add typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import Typography from "@/components/Typography";
 
 export default function Home() {
   return (
-    <div>
-      <Typography variant="h1" font="body"> testing body</Typography>
-      <Typography variant="h1" font="title" > testing title</Typography>
-    </div>
-    
+    <div className="bg-secondary w-full min-h-screen"></div>
   );
 }
