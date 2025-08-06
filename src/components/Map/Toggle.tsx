c7ef17c997e5c6440e17a31b6c64a772e9e08ae8
fioreenza <fiorenzanalle05@gmail.com>
2025-08-06 13:42:09 +0700

    feat: toggle for mobile

--- src/components/Map/Toggle.tsx
+++ src/components/Map/Toggle.tsx
@@ -32,23 +32,23 @@ export default function Toggle ({showAQILayer, setShowAQILayer, showPLTULayer, s
     }
 
     return (
-        <div className="bg-secondary w-fit space-y-6 h-fit px-6 justify-center items-center flex flex-col rounded-l-xl py-8 absolute top-48 z-10 right-0">
-             <button onClick={clickAQI}  className={showAQILayer ? "bg-primary-600 px-4 py-1 rounded-full border-1 border-secondary"  : "bg-secondary px-4 py-1 rounded-full border-1 border-primary-600"}>
+        <div className="bg-secondary md:w-48 md:space-y-6 h-fit px-6 justify-center items-center flex-col md:rounded-l-xl py-8 absolute bottom-0 grid grid-cols-2 gap-4 md:flex rounded-t-xl w-full md:top-48 md:z-10 md:right-0">
+             <button onClick={clickAQI}  className={showAQILayer ? "bg-primary-600 w-full py-1 rounded-full border-1 border-secondary"  : "bg-secondary w-full py-1 rounded-full border-1 border-primary-600"}>
                 <Typography variant="bs" font="body" className={showAQILayer ?"text-secondary":"text-black"}>
                     Kualitas Udara
                 </Typography>
             </button>
-             <button onClick={clickPLTU}  className={showPLTULayer ? "bg-primary-600 px-4 py-1 rounded-full border-1 border-secondary"  : "bg-secondary px-4 py-1 rounded-full border-1 border-primary-600"}>
+             <button onClick={clickPLTU}  className={showPLTULayer ? "bg-primary-600 w-full py-1 rounded-full border-1 border-secondary"  : "bg-secondary w-full py-1 rounded-full border-1 border-primary-600"}>
                 <Typography variant="bs" font="body" className={showPLTULayer ?"text-secondary":"text-black"}>
                     PLTU/Pabrik
                 </Typography>
             </button>
-            <button onClick={clickForest}  className={showForestLayer ? "bg-primary-600 px-4 py-1 rounded-full border-1 border-secondary"  : "bg-secondary px-4 py-1 rounded-full border-1 border-primary-600"}>
+            <button onClick={clickForest}  className={showForestLayer ? "bg-primary-600 w-full py-1 rounded-full border-1 border-secondary"  : "bg-secondary w-full py-1 rounded-full border-1 border-primary-600"}>
                 <Typography variant="bs" font="body" className={showForestLayer ?"text-secondary":"text-black"}>
                     Tutupan Hutan
                 </Typography>
             </button>
-            <button onClick={clickReport}  className={showReportLayer ? "bg-primary-600 px-4 py-1 rounded-full border-1 border-secondary"  : "bg-secondary px-4 py-1 rounded-full border-1 border-primary-600"}>
+            <button onClick={clickReport}  className={showReportLayer ? "bg-primary-600 w-full py-1 rounded-full border-1 border-secondary"  : "bg-secondary w-full py-1 rounded-full border-1 border-primary-600"}>
                 <Typography variant="bs" font="body" className={showReportLayer ?"text-secondary":"text-black"}>
                     Report Warga
                 </Typography>
